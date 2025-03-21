93bd54bbb9ee4f6f796641325621184c493eeee9
bioinfo.guru <101492537+bioinfoguru@users.noreply.github.com>
2025-03-21 13:22:14 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vuetify_project/' : '/',
+
   plugins: [
     VueRouter(),
     Vue({
